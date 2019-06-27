82b6a456742f1ced2930e89958fe56285be0fd6b
Jofiag <Aguemon.wahab.97@gmail.com>
2019-06-27 22:14:46 +0200

    Implémentation de la classe principale Main

--- src/jofiag/store/Main.java
+++ src/jofiag/store/Main.java
@@ -3,6 +3,18 @@ package jofiag.store;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car Bugatti = new Car("Buggati", "Bleue", 2_000_000);
+        Car CamaroSS = new Car("CamaroSS", "Noir malt", 50_000);
+        Car MustandGT = new Car("MustangGT", "Blan bleue", 100_000);
+        Car Muciellaguo = new Car("Muciellago", "Vert", 500_000);
+        Car Gaillardo = new Car("Gaillardo", "Jaune", 400_000);
+        Car Porche = new Car("Porche", "Gris", 30_000);
+
+        System.out.println(Bugatti);
+        System.out.println(CamaroSS);
+        System.out.println(MustandGT);
+        System.out.println(Muciellaguo);
+        System.out.println(Gaillardo);
+        System.out.println(Porche);
     }
 }
